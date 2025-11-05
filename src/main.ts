657f84b9a65485557f08a9af33a5a0f490e3093c
Linda <lindakjellberg7@gmail.com>
2025-11-05 13:56:12 +0100

    Add a paragraph and append it to app

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,17 @@
 import './style.css'
 
+const p = document.createElement("p");
+
+/* If I will need to access app through the DOM more than once I should create a variable for app,
+because each time I access the HTML-element div (with the id app) the DOM will reload it.
+So for performance reasons it is good practice to create a variable for app because then I will use the variable to acces app instead,
+and the DOM will only need to load it once but I can reuse it as much as I want.
+
+However if I only need to access it once, and know I this for sure, 
+then creating a variable will be unneccessary since I will have an unused variable declared...
+*/
+
+//const app = 
+document.getElementById("app")?.appendChild(p);
+
+console.log(p)
